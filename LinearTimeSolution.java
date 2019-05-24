dec4c81ffec18a3a2ae1bcb870421e2b67d03229
Eric Schenck <38293287+snowfire5492@users.noreply.github.com>
2019-05-23 19:45:08 -0700

    Update LinearTimeSolution.java

--- LinearTimeSolution.java
+++ LinearTimeSolution.java
@@ -3,21 +3,17 @@ public class LinearTimeSolution {
 
 	// [ 1, 2, 3, 4, 5 ]
 	
-	// create pre-product array  [ 1,         1*2,     1*2*3,   
-1*2*3*4, 1*2*3*4*5 ]
+	// create pre-product array  [ 1,         1*2,     1*2*3,   1*2*3*4, 1*2*3*4*5 ]
 	
-	// create post-product array [ 1*2*3*4*5, 2*3*4*5, 3*4*5,   4*5,     
-5 ] 
+	// create post-product array [ 1*2*3*4*5, 2*3*4*5, 3*4*5,   4*5,     5 ] 
 	
-	// combine to createn result [ 2*3*4*5,   1*3*4*5, 1*2*4*5, 
-1*2*3*5, 1*2*3*4 ]
+	// combine to createn result [ 2*3*4*5,   1*3*4*5, 1*2*4*5, 1*2*3*5, 1*2*3*4 ]
 	
 	public int[] productOfArray(int[] arr) {
 		
 		int arraySize = arr.length;
-		int[] result = new int[arraySize], pre = new 
-int[arraySize], 
-				post = new int[arraySize];
+		int[] result = new int[arraySize], pre = new int[arraySize], 
+			post = new int[arraySize];
 		
 		for(int i= 0; i < arraySize; ++i) {
 			
